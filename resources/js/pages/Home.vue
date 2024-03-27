35f4883b6ecaf1bead2d9b8cf33fb0f8c0af588d
rgrttg <ottiger@outlook.de>
2024-03-27 14:45:58 +0100

    first commit

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Startseite</h1>
+        <router-link to="/login">Link zur Login Seite</router-link>
+    </div>
+</template>
---
+++ resources/js/router.js
@@ -0,0 +1,25 @@
+
+
+
+export const routes = [
+    {
+        path: "/",
+        component: () => import("./pages/Home.vue"),
+    },
+    
+    {
+        path: "/login",
+        component: () => import("./pages/Auth/Login.vue"),
+    },
+
+    {
+        path: "/dashboard",
+        component: () => import("./pages/Dashboard.vue"),
+        meta: { requiresAuth: true },
+    },
+];
+
+
+
+
+
