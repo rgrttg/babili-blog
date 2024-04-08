a079df089491d6996d61c6921571e85c487dd2a9
[github-wojtek-strzyzowski] <[github-strzyzowski@gmx.de]>
2024-04-08 16:22:20 +0200

    card= title, img, icons placeholder, subtitle, pragraph

--- resources/js/pages/SingleBlog.vue
+++ resources/js/pages/SingleBlog.vue
@@ -9,7 +9,7 @@ const loadBlog = async () => {
   try {
     const response = await axios.get('/api/blogs/detail/2'); // Beispiel: ID 2
     blog.value = await response.data;
-    console.log(response.data);
+    // console.log(response.data);
   } catch (error) {
     console.error('Fehler beim Laden des Blogs:', error);
   }
@@ -26,15 +26,64 @@ onMounted(() => {
 </script>
 
 <template>
-  <div v-if="blog">
-    <h1>{{ blog.title }}</h1>
-    <p>{{ blog.content }}</p>
+  
+     <!-- <div class="header"> -->
+    <!-- //hier kommt der header -->
+  <!-- </div> -->
+
+<div class="card">
+
+  <div class="title" v-if="blog">
+      <h1>{{ blog.title }}</h1>
+  </div> 
+
+  <div class="user-details">
+    <div class="image">
+        <img v-if="blog?.image_url" :src="blog?.image_url" class="profile-image"/>
+    </div>
+    <div class="socials">
+      SOCIAL ICONS
+    </div>
   </div>
+      
+  <div v-if="blog?.content" v-for="(entry,index) in blog.content" :key="index">
+     
+        <h2 v-if="entry.type='subtitle'">{{ entry.value }}</h2> 
+     
+        <p v-if="entry.type='paragraph'">{{ entry.value }}</p>
+    
+  </div>
+
+  
+</div>
+
 </template>
  
 <style scoped>
-h1, p {
-    color: white;
+
+h1,h2, p {
+    color: black;
+}
+
+.card {
+  background-color: gainsboro;
+  display: flex;
+  flex-direction: column;
+  padding: 10%;
+}
+
+.user-details {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.profile-image {
+  width: 100px; /* Ändern Sie die Breite und Höhe nach Bedarf */
+  height: 100px;
+  border-radius: 50%; /* Rundes Bild */
+  object-fit: cover; /* Das Bild wird in das festgelegte Rechteck gezoomt, um es zu füllen */
 }
 
 </style>
