ad88dc291b7c167973305c5a9b3042cd00ad327f
[github-wojtek-strzyzowski] <[github-strzyzowski@gmx.de]>
2024-04-08 13:38:29 +0200

    single-blog am arbeiten

---
+++ resources/js/pages/SingleBlog.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+// import { convertToHtml } from '@/components/Creator.vue';
+
+const blog = ref(null);
+
+const loadBlog = async () => {
+  try {
+    const response = await axios.get('/api/blogs/detail/2'); // Beispiel: ID 2
+    blog.value = await response.data;
+    console.log(response.data);
+  } catch (error) {
+    console.error('Fehler beim Laden des Blogs:', error);
+  }
+};
+
+// const someMethod = () => {
+//   const html = convertToHtml(blog.content.value);
+//   // Do something with the generated HTML
+// };
+
+onMounted(() => {
+  loadBlog();
+});
+</script>
+
+<template>
+  <div v-if="blog">
+    <h1>{{ blog.title }}</h1>
+    <p>{{ blog.content }}</p>
+  </div>
+</template>
+ 
+<style scoped>
+h1, p {
+    color: white;
+}
+
+</style>
