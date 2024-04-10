46f33afb16fdc3dc9543cfcd70dc27a20fd8e968
mlavin7 <mlavin7@gmail.com>
2024-04-10 14:54:43 +0200

    router

--- resources/js/router.js
+++ resources/js/router.js
@@ -4,33 +4,70 @@
 export const routes = [
     {
         path: "/",
+        name:"home",
         component: () => import("./pages/Home.vue"),
+        meta: { showLogo: false, allBlogs: true, getStarted: true, home: true},
     },
     
     {
         path: "/login",
+        name:"login",
         component: () => import("./pages/Login.vue"),
+        meta: { showLogo: true, allBlogs: false, getStarted: false, home: true},
     },
 
     {
         path: "/dashboard",
+        name:"dashboard",
         component: () => import("./pages/Dashboard.vue"),
-        meta: { requiresAuth: true },
+        meta: { requiresAuth: true, 
+        showLogo: true, allBlogs:true , getStarted: false, home: true},
+    
     },
     {
         path: "/register",
+        name:"register",
         component: () => import("./pages/Register.vue"),
+        meta: { showLogo: true, allBlogs:false, getStarted: false, home: true}
     },
 
     {
         path:"/create",
+        name:"createPost",
         component: () => import("./pages/CreateBlog.vue"),
+        meta: { showLogo: true, allBlogs:false, getStarted: false, home: true}
     },
 
     {
         path:"/single-blog",
+        name:"singleBlog",
         component: () => import("./pages/SingleBlog.vue"),
-    }
+        meta: { showLogo: true, allBlogs:true, getStarted: false, home: true}
+    },
+    {
+        path:"/edit-profile",
+        name:"editProfile ", 
+        component: () => import("./pages/EditProfile.vue"),
+        meta: { showLogo: true, allBlogs:false, getStarted: false, home: true}
+    },
+    
+    {
+        path:"/alle-blogs",
+        name: "allBlogs", 
+        component: () => import("./pages/AllBlog.vue"),
+        meta: { showLogo: true, allBlogs:false, getStarted: false, home: true}
+    },
+
+
+    {
+        path:"/edit-blog",
+        name:"editBlog",
+        component: () => import("./pages/EditBlog.vue"),
+        meta: { showLogo: true, allBlogs:false, getStarted: false, home: true},
+
+    },
+
+  
 ];
 
 
