5947aa0a56f24d2cd4e945e3f57c5d2eae8e9e8f
rgrttg <ottiger@outlook.de>
2024-04-10 12:13:58 +0200

    Routes Kosmetik

--- routes/api.php
+++ routes/api.php
@@ -12,25 +12,25 @@ Route::get('test', function () {
     return "test";
 });
 // Blog per Id
-Route::get('blogs/detail/{id}', [BlogController::class, 'mitOhneAllesScharf']);
+Route::get('/blogs/detail/{id}', [BlogController::class, 'mitOhneAllesScharf']);
 // drei neuste
-Route::get('blogs/latest-three', [BlogController::class, 'latestThreeBlogs']);
+Route::get('/blogs/latest-three', [BlogController::class, 'latestThreeBlogs']);
 // alle neuste
-Route::get('blogs/all-latest', [BlogController::class, 'allBlogsByLatest']);
+Route::get('/blogs/all-latest', [BlogController::class, 'allBlogsByLatest']);
 // alle älteste
-Route::get('blogs/all-oldest', [BlogController::class, 'allBlogsByOldest']);
+Route::get('/blogs/all-oldest', [BlogController::class, 'allBlogsByOldest']);
 // drei meiste commentare
-Route::get('blogs/most-interactions-three', [BlogController::class, 'mostInteractionsThreeBlogs']);
+Route::get('/blogs/most-interactions-three', [BlogController::class, 'mostInteractionsThreeBlogs']);
 // alle meiste commentare
-Route::get('blogs/most-interactions-all', [BlogController::class, 'mostInteractionsAllBlogs']);
+Route::get('/blogs/most-interactions-all', [BlogController::class, 'mostInteractionsAllBlogs']);
 // alle wenigste commentare
-Route::get('blogs/least-interactions-all', [BlogController::class, 'leastInteractionsAllBlogs']);
+Route::get('/blogs/least-interactions-all', [BlogController::class, 'leastInteractionsAllBlogs']);
 
 // Route zum Abrufen von Blogs anhand von Tags.
 // Diese Route akzeptiert entweder einen einzelnen Tag oder ein Array von Tags.
 // Beispiel mit einem einzelnen Tag: /api/blogs/by-tags?tags=ipsum
 // Beispiel mit einem Array von Tags: /api/blogs/by-tags?tags[]=ipsum&tags[]=lorem
-Route::get('blogs/by-tags', [BlogController::class, 'getBlogsByTags']);
+Route::get('/blogs/by-tags', [BlogController::class, 'getBlogsByTags']);
 
 
 
@@ -66,7 +66,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     // Diese Route ermöglicht authentifizierten Benutzern das Bewerten eines Blogs mit einer positiven oder negativen Bewertung.
     // Beispielanfrage: POST /api/blogs/{BlogId}/rate
     // Beispiel request body: { "rating": 1 } (für positiv) oder { "rating": 0 } (für negativ)
-    Route::post('blogs/rate/{id}', [BlogController::class, 'rateBlog']);
+    Route::post('/blogs/rate/{id}', [BlogController::class, 'rateBlog']);
 
     // Diese Route ermöglicht authentifizierten Benutzern 
     // einen Kommentar zu einem Blog (id) zu schreiben /ro
