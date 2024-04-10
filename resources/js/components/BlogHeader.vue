01a8e438aa0e8aa7088b1ed53c4d9b25cd926f3f
rgrttg <ottiger@outlook.de>
2024-04-10 11:56:45 +0200

    Blogheader.vue hinzugefügt

---
+++ resources/js/components/BlogHeader.vue
@@ -0,0 +1,158 @@
+<script setup>
+import { useRoute } from 'vue-router';
+import { useAuthStore } from "../stores/AuthStore"
+
+const route = useRoute();
+const store = useAuthStore();
+
+
+
+
+</script>
+<template>
+
+    <header>
+        <div id="header">
+            <div class="left">
+                <img v-if="route?.meta?.showLogo" src="/public/img/Logo-new.svg" alt="Tech Blog Logo" id="logo">
+
+            </div>
+            <div class="right">
+                <ul class="menu">
+                    <li v-if="route?.meta?.allBlogs" id="blogs">
+                        <router-link to="/posts" customv-slot="{ navigate }">
+                            <div class="link" @click="navigate" role="link">
+                                Alle Blogs
+                            </div>
+                        </router-link>
+                    </li>
+
+                    <li v-if="!store?.authUser?.name" id="login">
+
+                        <router-link to="/login" customv-slot="{ navigate }">
+                            <div class="link" @click="navigate" role="link">
+                                Login
+                            </div>
+                        </router-link>
+                    </li>
+
+                    <li v-if="!store?.authUser?.name && !route?.meta?.getStarted" id="sign">
+                        <router-link :to="{name:'register'}" customv-slot="{ navigate }">
+                            <div class="link" @click="navigate" role="link">
+                                Sign in
+                            </div>
+                        </router-link>
+                    </li>
+
+                    <li v-if="store?.authUser?.name" id="myProfile">
+                        <router-link to="/dashboard" customv-slot="{ navigate }">
+                            <div class="link" @click="navigate" role="link">
+
+                                My Profile
+                            </div>
+                        </router-link>
+                    </li>
+
+                    <li v-if="route?.meta?.getStarted" id="getStart">
+                      <router-link to="/register" customv-slot="{ navigate }">
+                            <div class="link" @click="navigate" role="link">
+                                <div class="link" id="getStartedButton">
+                                    Get started
+                                </div>
+                            </div>
+                        </router-link>
+                    </li>
+                
+
+
+                </ul>
+            </div>
+        </div>
+    </header>
+
+</template>
+<style scoped>
+#header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-items: center;
+    height: 63px;
+    width: 100vw;
+    /* background-image: linear-gradient(135DEG , rgb(222,7,241), rgb(5,5,5)); */
+    background-color: black;
+
+}
+
+.left {
+    width: 50vw;
+    height: 63px;
+}
+
+.right {
+    width: 50vw;
+    height: 63px;
+}
+
+.menu {
+    display: flex;
+    flex-direction: row;
+    justify-content: end;
+    list-style: none;
+    gap: 20px;
+    margin-right: 40px;
+    align-items: center;
+    height: 100%;
+    margin-bottom: 0px;
+    margin-top: 0px;
+    font-weight: bold;
+
+}
+
+.link {
+    text-decoration: none;
+    color: white;
+    font-family: Arial, sans-serif;
+
+
+}
+
+
+#getStartedButton {
+    border: 2px solid white;
+    border-radius: 30px;
+    background-color: black;
+    padding: 10px;
+    font-weight: bold;
+}
+
+#logo {
+    height: 70px;
+}
+
+@media only screen and (max-width: 600px) {
+    #logo {
+        height: 40px;
+    }
+
+    .menu {
+        gap: 5px;
+        margin-right: 5px;
+        align-items: center;
+        height: 100%;
+        margin-bottom: 0px;
+        margin-top: 0px;
+        font-weight: bold;
+        font-size: smaller;
+
+    }
+
+    #getStartedButton {
+        border: 2px solid white;
+        border-radius: 30px;
+        background-color: black;
+        padding: 2px;
+        font-weight: bold;
+    }
+}
+</style>
