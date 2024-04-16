9df17a500a5866612bf184dd45e9ee85e1dadb23
melketesfay <melketesfay@gmail.com>
2024-04-16 12:39:22 +0200

    allblogs

--- resources/js/pages/AllBlog.vue
+++ resources/js/pages/AllBlog.vue
@@ -1,10 +1,36 @@
 <script setup>
-import BlogHeader from '../components/BlogHeader.vue'; 
+import BlogHeader from "../components/BlogHeader.vue";
+import Card from "../components/Card.vue";
+import SearchBar from "../components/SearchBar.vue";
+import { ref, onMounted, defineProps } from "vue";
+import axios from "axios";
+
+const blogs = ref([]);
+
+const load = async () => {
+    try {
+        const response = await axios.get("/api/blogs/all-latest");
+        blogs.value = response.data;
+        // console.log(response.data);
+    } catch (error) {
+        console.error("Error loading blogs:", error);
+    }
+};
+
+function getSuchInput(suchInhalt) {
+    console.log(suchInhalt);
+}
+
+onMounted(() => {
+    load();
+});
 </script>
-
 <template>
-<BlogHeader/>
-</template>
+    <BlogHeader />
+    <SearchBar v-on:suchen="getSuchInput" />
 
-<style scoped>
-</style>
+    <div>
+        <Card :card-content="blogs" />
+    </div>
+</template>
+<style scoped></style>
