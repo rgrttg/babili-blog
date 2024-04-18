a2d30d121d1537966956c707a6f9bb7dc8897221
[github-wojtek-strzyzowski] <[github-strzyzowski@gmx.de]>
2024-04-18 09:09:34 +0200

    edit angepasst

--- resources/js/pages/EditBlog.vue
+++ resources/js/pages/EditBlog.vue
@@ -36,45 +36,50 @@ onBeforeMount(() => {
     <!-- //hier kommt der header -->
   <!-- </div> -->
 <body>
+  
 <div class="card">
 
   <div class="card-container">
-  <div class="title" v-if="blog">
-      <h1>{{ blog?.title }}</h1>
-  </div> 
+    <div class="title" v-if="blog">
+        <h1>{{ blog?.title }}</h1>
+    </div> 
 
-  <div class="description" v-if="blog">
-    <p>{{ blog?.description }}</p>
-  </div>
-
-  <div class="user-details">
-    <div class="image">
-            <img v-if="blog?.profile_picture" :src="blog?.profile_picture" class="profile-picture"/>
-        <div class="author-info">
-            <span v-if="blog">{{ blog?.author_name }} </span>&nbsp;
-            <span v-if="blog">{{ blog?.published_at }}</span>
-        </div>
+    <div class="description" v-if="blog">
+      <p>{{ blog?.description }}</p>
     </div>
+
+    <div class="user-details">
+      <div class="image">
+              <img v-if="blog?.profile_picture" :src="blog?.profile_picture" class="profile-picture"/>
+          <div class="author-info">
+              <span v-if="blog">{{ blog?.author_name }} </span>&nbsp;
+              <span v-if="blog">{{ blog?.published_at }}</span>
+          </div>
+      </div>
     
-    <div class="socials">
-      SOCIAL ICONS
-    </div>
+      <div class="socials">
+        SOCIAL ICONS
+      </div>
+  </div>
   </div>
       
-
-    <creator v-if="blog?.content" :content="blog?.content" @saved="getJson"/>
-
-    
-
-  
- 
-
+    <div v-if="blog?.content" v-for="(entry,index) in blog.content" :key="index">
+      
+          <h2 v-if="entry.type=='subheader'">{{ entry.value }}</h2> 
+      
+          <p v-if="entry.type=='paragraph'">{{ entry.value }}</p>
+      
+    </div>
+  <div>
+      <creator v-if="blog?.content" :content="blog?.content" @saved="getJson"/>
+  </div>
 </div>
+
 <!-- <div>
     <creator @saved="getJson"/>
 </div> -->
   
-</div>
+
 </body>
 </template>
  
